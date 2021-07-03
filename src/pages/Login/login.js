9ff757559eb7e92431b73c05ea37819cbf1477e4
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-07-02 23:07:55 -0300

    Front já comunicável com API

---
+++ src/pages/Login/login.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+
+function fazerLogin(login, senha) {
+    const instance = axios.create({
+        baseURL: 'http://localhost:3001',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+        }
+    });
+    console.log('teste');
+
+    return instance.post('/auth/authenticate', 
+        {
+            "login": login,
+            "senha": senha
+        }
+    );
+}
+
+
+
+export default fazerLogin;
