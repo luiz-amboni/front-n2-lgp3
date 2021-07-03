25784f0129c4593a6573f297dbcac5865145430d
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-07-03 15:06:38 -0300

    Show URL API variable

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-
 function fazerLogin(login, senha) {
     const instance = axios.create({
-        baseURL: process.env.URL_API || 'http://localhost:3001',
+        baseURL: 'https://api-n3.herokuapp.com',
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json',
         }
     });
-    console.log('teste');
 
     return instance.post('/auth/authenticate', 
         {
